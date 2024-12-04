d397eec232ced26c7c1088b67baf55bb0606b3a8
vishwas.ningappa <vishwas.ningappa@BDC7-L-1HDSQG3>
2024-12-04 21:43:18 +0530

    Add project files.

---
+++ Basic_Counting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your number to start counting: \n");
+        int Input = int.Parse(Console.ReadLine());
+        int i = 0;
+        while (i <= Input)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+    }
+}
